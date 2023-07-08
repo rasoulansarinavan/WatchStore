83c93da0593925a93867c2a7c453291e71f9333d
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-08 08:20:51 +0330

    add Permission trait in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,12 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
