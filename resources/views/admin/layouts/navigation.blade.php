c0f2b71f55f6b91281969896421e98b59dc17912
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-08 11:24:56 +0330

    update navigation for routes

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -1,12 +1,22 @@
 <div class="navigation">
     <div class="navigation-icon-menu">
         <ul>
+
             <li data-toggle="tooltip" title="کاربران">
                 <a href="#users" title=" کاربران">
                     <i class="icon ti-user"></i>
                 </a>
             </li>
+
+
+            <li data-toggle="tooltip" title="محصولات">
+                <a href="#products" title="محصولات">
+                    <i class="icon ti-folder"></i>
+                </a>
+            </li>
+
         </ul>
+
         <ul>
             <li data-toggle="tooltip" title="ویرایش پروفایل">
                 <a href="#" class="go-to-page">
@@ -27,6 +37,35 @@
                 <ul>
                     <li><a href="{{route('users.create')}}">ایجاد کاربر</a></li>
                     <li><a href="{{route('users.index')}}">لیست کاربران</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">نقش ها</a>
+                <ul>
+                    <li><a href="{{route('roles.create')}}">ایجاد نقش</a></li>
+                    <li><a href="{{route('roles.index')}}">لیست نقش ها</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">لاگ ها</a>
+                <ul>
+                    <li><a href="{{route('logs')}}">لیست لاگ ها</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul id="products">
+            <li>
+                <a href="#">دسته بندی</a>
+                <ul>
+                    <li><a href="{{route('category.create')}}">ایجاد دسته بندی</a></li>
+                    <li><a href="{{route('category.index')}}">لیست دسته بندی</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">اسلایدر</a>
+                <ul>
+                    <li><a href="{{route('sliders.create')}}">ایجاد اسلایدر</a></li>
+                    <li><a href="{{route('sliders.index')}}">لیست اسلایدر</a></li>
                 </ul>
             </li>
         </ul>
