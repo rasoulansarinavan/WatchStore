3f4d86dae674a8019c448b45cf376ec12d858f40
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-11 07:46:03 +0330

    add PropertyGroupController

---
+++ app/Http/Controllers/Admin/PropertyGroupController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\PropertyGroup;
+use Illuminate\Http\Request;
+
+class PropertyGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $title = "لبست گروه ویژگی ها";
+        return view('admin.property_group.list', compact('title'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $title = "ایجاد گروه ویژگی ها";
+        return view('admin.property_group.create', compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        PropertyGroup::query()->create([
+            'title' => $request->input('title')
+        ]);
+        return redirect()->route('property_groups.index')->with('گروه ویژگی ها با موفقیت انجام شد');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $property_group = PropertyGroup::query()->find($id);
+        $title = "ویرایش گروه ویژگی ها";
+        return view('admin.property_group.edit', compact('property_group', 'title'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $property_group = PropertyGroup::query()->find($id);
+        $property_group->update([
+            'title' => $request->input('title')
+        ]);
+        return redirect()->route('property_groups.index')->with('گروه ویژگی ها با موفقیت انجام شد');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
