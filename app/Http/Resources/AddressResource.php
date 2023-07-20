9b5ccec2f13ef435f2badeb57ba5e84a1ba37910
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-20 08:40:19 +0330

    change codes in AddressResource in Resources

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -14,6 +14,11 @@ class AddressResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'address'=>$this->address,
+            'postal_code'=>$this->postal_code,
+            'lat'=>$this->lat,
+            'lang'=>$this->lang,
+        ];
     }
 }
