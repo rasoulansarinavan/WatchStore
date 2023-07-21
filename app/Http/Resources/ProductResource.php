e0389dbf60af6c1493f4cff5ab743e6ef9c6b1db
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-21 12:18:06 +0330

    add ProductResource in Resources

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'title_en' => $this->title_en,
+            'slug' => $this->slug,
+            'price' => $this->price,
+            'review' => $this->review,
+            'count' => $this->count,
+            'image' => url('images/admin/products/big/' . $this->image),
+            'guaranty' => $this->guaranty,
+            'discount' => $this->discount,
+            'description' => $this->description,
+            'is_special' => $this->is_special,
+            'special_expiration' => $this->special_expiration,
+            'status' => $this->status,
+            'category_id' => $this->category_id,
+            'category' => $this->category->title,
+            'brand_id' => $this->brand_id,
+            'brand' => $this->brand->title
+        ];
+    }
+}
