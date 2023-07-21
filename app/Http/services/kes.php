a6e5fe72f8071ca932429d52e575549eda94152e
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-21 12:16:27 +0330

    add kes in services

---
+++ app/Http/services/kes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\services;
+
+class kes
+{
+    const sliders = 'sliders';
+    const categories = 'categories';
+    const amazing_products = 'amazing_products';
+    const banner = 'banner';
+    const most_seller_products = 'most_seller_products';
+    const newest_products = 'newest_products';
+}
