be816dfdb548c1530a541400088c24ea3d908037
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-22 21:56:03 +0330

    add some key for products

--- app/Http/services/kes.php
+++ app/Http/services/kes.php
@@ -9,5 +9,8 @@ class kes
     const amazing_products = 'amazing_products';
     const banner = 'banner';
     const most_seller_products = 'most_seller_products';
+    const most_viewed_products = 'most_viewed_products';
+    const cheapest_products = 'cheapest_products';
+    const most_expensive_products = 'most_expensive_products';
     const newest_products = 'newest_products';
 }
