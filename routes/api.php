d483108c334aabff27e83423b6792896238b4de5
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-07-22 21:55:41 +0330

    refactor api.php add Product routes

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,15 @@ Route::prefix('/v1')->namespace('Api\V1')->group(function () {
     Route::post('send_sms', [\App\Http\Controllers\Api\V1\AuthApiController::class, 'sendSms']);
     Route::post('verify_sms_code', [\App\Http\Controllers\Api\V1\AuthApiController::class, 'verifySms']);
     Route::get('/home', [\App\Http\Controllers\Api\v1\HomeApiController::class, 'home']);
+    Route::get('/most_sold_products', [\App\Http\Controllers\Api\v1\ProductsApiController::class, 'most_sold_products']);
+    Route::get('/most_viewed_products', [\App\Http\Controllers\Api\v1\ProductsApiController::class, 'most_viewed_products']);
+    Route::get('/newest_products', [\App\Http\Controllers\Api\v1\ProductsApiController::class, 'newest_products']);
+    Route::get('/cheapest_products', [\App\Http\Controllers\Api\v1\ProductsApiController::class, 'cheapest_products']);
+    Route::get('/most_expensive_products', [\App\Http\Controllers\Api\v1\ProductsApiController::class, 'most_expensive_products']);
 });
+
 Route::prefix('/v1')->namespace('Api\V1')->middleware('auth:sanctum')->group(function () {
     Route::post('register', [\App\Http\Controllers\Api\V1\UserApiController::class, 'register']);
-
 });
 
 
